f50e86a6df6b43f49d43110eff5567bdc017fbcc
Yashsangwan2002 <77121057+Yashsangwan2002@users.noreply.github.com>
2021-07-09 17:34:41 +0530

    wqdqwd

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/dasheee/index.html").status(200);
+  res.sendFile(__dirname + "/dasheee/index.html");
 });
 
 app.use("/auth", authRoutes);
